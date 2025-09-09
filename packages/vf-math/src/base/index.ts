e924fdd9e206174593a8ae3121878952e1e85698
wuyifan <wuyifan@udschina.com>
2025-09-09 17:58:53 +0800

    feat(math): :sparkles: 更新math导出

--- packages/vf-math/src/base/index.ts
+++ packages/vf-math/src/base/index.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: wuyifan wuyifan@udschina.com
+ * @Date: 2025-08-21 17:05:45
+ * @LastEditors: wuyifan wuyifan@udschina.com
+ * @LastEditTime: 2025-09-09 16:30:40
+ * @FilePath: \vf-studio\packages\vf-math\src\base\index.ts
+ * Copyright (c) 2024 by wuyifan email: 1208097313@qq.com, All Rights Reserved.
+ */
 export * from './AbstractMathObject';
 export * from './Vector2';
 export * from './Vector3';
@@ -5,6 +13,7 @@ export * from './Matrix2';
 export * from './Matrix3';
 export * from './Matrix4';
 export * from './Quaternion';
+export * from './Euler';
 export * from './Sphere';
 export * from './Box2';
 export * from './Box3';
